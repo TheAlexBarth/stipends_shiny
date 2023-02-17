088077398a364cb46db88725be891225317baf27
Alex Barth <abarth1014@gmail.com>
2023-02-17 15:19:22 -0500

    complete app and upload to shinyapps.io

--- R/stipendCompApp.R
+++ R/stipendCompApp.R
@@ -11,7 +11,10 @@ comp_ui <- fluidPage(
     ),
     mainPanel(
       plotOutput('comparison_plot'),
-      tableOutput('table')
+      textOutput('comparison_summary'),
+      tags$head(tags$style('#comparison_summary{font-size: 20px;
+                                                font-style: bold;
+                                                text-align: center}'))
     )
   )
 )
@@ -19,7 +22,6 @@ comp_ui <- fluidPage(
 comp_server <- function(input, output, session) {
   require(ggplot2)
   require(ggpubr)
-  require(ggusc)
   require(ggpubr)
   require(ggtext)
   # Build data, would be replaced by the csv loading in your case
@@ -53,13 +55,23 @@ comp_server <- function(input, output, session) {
   
   output$comparison_plot <- renderPlot({
     
+    if(length(input$selected_comp) <= 1) {
+      title_lab <- paste0('Graduate Stipend Comparison for ', 
+                           input$selected_comp, ' Students')
+    } else {
+      title_lab <- paste0('Graduate Stipend Comparison for ', 
+                          paste0(input$selected_comp, collapse = ' & '),
+                          ' Students')
+    }
+    
+    
     ggplot() +
       geom_point(data = comparison_data(),
                  aes(x = living, y = stipend),
                  color = 'grey', alpha = 0.5) +
       geom_point(data = usc_data(),
                  aes(x = 35609.6, y = mean),
-                 color = usc_cols(1)) +
+                 color = '#73000a') +
       stat_smooth(data = comparison_data(),
                   aes(x = living, y = stipend),
                   color = 'black', method = 'lm', se = F) +
@@ -72,16 +84,36 @@ comp_server <- function(input, output, session) {
                   fullrange = T,
                   color = 'red', method = 'lm', se = F)+
       labs(x = 'MIT Living Wage [$]', y = 'Graduate Annual Stipend [$]',
-           title = paste0('Graduate Stipend Comparison for ', 
-                          input$selected_comp, ' Students'),
-           subtitle = "Comparison universities are shown in grey. 
-           USC stipends are shown in <span style='color:#73000a;'>**garnet**</span>")+
-      theme_pubr() +
+           title = title_lab, # need to change for multiple inputs
+           subtitle = "Comparison universities are shown in <span style='color: #999999;'>**grey**</span>.
+           <br>
+           USC stipends are shown in <span style='color:#73000a;'>**garnet**</span>
+           <br>
+           The <span style='color: green;'>**green**</span> line indicates the MIT cost of living for a single person
+           <br>
+           The <span style='color: #FF0000;'>**red**</span> line indicates the poverty line.")+
+      theme_pubr(base_size = 15) +
       theme(plot.subtitle = element_markdown())
   })
   
-}
-
-compStipendApp <- function() {
-  shinyApp(ui = comp_ui, server = comp_server)
+  
+  output$comparison_summary <- renderText({
+    
+    if(length(comparison_data() > 0)) {
+      
+      mod_selected <- lm(stipend ~ living, data = comparison_data())
+      
+      predicted_salary <- summary(mod_selected)$coef[2] * 35609.6 + summary(mod_selected)$coef[1]
+      
+      average_usc <- mean(usc_data()$mean)
+      
+      if(predicted_salary >= average_usc) {
+        paste0('Adjusting for the cost of living, the average USC stipend is $', 
+               round(predicted_salary - average_usc, 2),
+               ' lower than other universities!')
+      } else {
+        "This is a rare case where USC stipends exceed the average competitor"
+      }
+    }
+  })
 }
